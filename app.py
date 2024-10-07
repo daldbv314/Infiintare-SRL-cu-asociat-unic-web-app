3c6f52bfd2ae00df71e19f0041567d4e6beb9f1f
Daniel Aldea <daniel.aldea85@gmail.com>
2024-10-07 15:49:40 +0300

    updated requirements. starting dev.

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+import io
+import datetime
+import streamlit as st
+from zipfile import ZipFile
+from pathlib import Path
+from docxtpl import DocxTemplate
+from dotenv import load_dotenv
+from io import BytesIO
+
+def main():
+
+
+
+
+
+
+    if __name__ == "__main__":
+        main()
